649214f9c37ae18b131689e839f5297230d966b4
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-03-14 11:25:26 +0530

    UC5-Commited

--- src/test/java/DBTest.java
+++ src/test/java/DBTest.java
@@ -18,4 +18,35 @@ public class DBTest{
             e.printStackTrace();
         }
     }
+    @Test
+    void givenData_shouldRetrieveData_byName() {
+        try {
+            a = DataBase.connected();
+            String expected = DataBase.reteriveDataByName(a);
+            Assert.assertEquals("30000", expected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void givenParticularDateRange_shouldReturn_correctRecords() {
+        try {
+            a = DataBase.connected();
+            String expected = DataBase.particularDateRange(a);
+            Assert.assertEquals("Rohit", expected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void givenGender_shouldReturn_SumSalary() {
+        try {
+            a = DataBase.connected();
+            String expected = DataBase.sumByGroup(a);
+            Assert.assertEquals("110000.0", expected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }

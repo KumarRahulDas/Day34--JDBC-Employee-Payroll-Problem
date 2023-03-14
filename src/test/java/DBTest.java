9d4014ba24bdd034b2bdfbcb36276d9b92c61c16
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-03-14 11:16:07 +0530

    Salary Updated

--- src/test/java/DBTest.java
+++ src/test/java/DBTest.java
@@ -1,17 +1,19 @@
-import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class DBTest extends TestCase {
+public class DBTest{
     static Connection a = null;
 
-    public void testReteriveData() {
+    @Test
+    public void givenUpdatedData_shouldRetrieve_correctRecords() {
         try {
             DataBase db = new DataBase();
             a = DataBase.connected();
-            String actual = DataBase.reteriveData(a);
-            Assert.assertEquals("Admin", actual);
+            String expected = DataBase.reteriveData(a);
+            Assert.assertEquals("30000", expected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
